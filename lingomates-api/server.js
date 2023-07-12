4ceca9386a411b2005200b217193f62de96a184b
Nat-FL <nrgraddy@gmail.com>
2023-07-12 10:07:02 -0700

    added more files and folders to backend/api

---
+++ lingomates-api/server.js
@@ -0,0 +1,21 @@
+const express = require("express"); //importing express
+const app = require("./app");
+const cors = require("cors"); //imorting cors
+const morgan = require("morgan"); // Import the Morgan middleware for logging
+const {PORT} = require("./config");
+const db = require("./db")
+
+
+// Middleware
+app.use(cors()); // Enable CORS middleware to handle cross-origin requests
+app.use(morgan("dev")); // Use Morgan middleware with 'dev' format for request logging
+app.use(express.json()); // Parse incoming requests with JSON payloads
+
+// Start the server
+//const PORT = 3001;
+
+//enabling the localhost at PORT - 3001
+app.listen(3001, () => {
+  //console logging the iniation of the server
+  console.log(`Server running  http://localhost:3001`);
+});
