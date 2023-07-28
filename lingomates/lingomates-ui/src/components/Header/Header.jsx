d5445401e64172e7e6d8a554e521bd31f699316f
Krystal Ohuabunwa <kco28@cornell.edu>
2023-07-28 09:49:58 -0700

    landing page

--- lingomates/lingomates-ui/src/components/Header/Header.jsx
+++ lingomates/lingomates-ui/src/components/Header/Header.jsx
@@ -1,48 +1,56 @@
-import './Header.css'
-import Login from '../Login/Login'
-import {useState} from 'react'
-import {Link} from 'react-router-dom'
-import { makeStyles } from '@mui/styles'
+import React from 'react';
+import "./Header.css"
+import { makeStyles } from '@mui/styles';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
-import { red } from '@mui/material/colors'
+import Button from '@mui/material/Button';
+import { red } from '@mui/material/colors';
+import { Slide } from "react-awesome-reveal";
+import { useState, useEffect } from 'react';
 
 
-// import SortIcon from '@mui/material/SortIcon';
+export default function Header() {
+  const [open, setOpen] = useState(false)
+  // const [scrollY, setScrollY] = useState(0)
+  const [navBarColor, setNavBarColor] = useState(false)
 
+  const handleToggle = () => {
+      setOpen(!open)
+  }
 
+  const listenScrollEvent = () => {
+      window.scrollY > 10 ? setNavBarColor(true) : setNavBarColor(false);
+  };
 
+  useEffect(() => {
+      window.addEventListener("scroll", listenScrollEvent);
+      return () => {
+          window.removeEventListener("scroll", listenScrollEvent);
+      };
+  }, []);
 
-const useStyles = makeStyles({
-    appbar:{
-        backgroundColor : "red",
-    },
-    appbarWrapper:{
-        // width: '80%',
-        // margin: '0 auto'
-    },
-    colorText: {
-        color: "brown"
-    }
-
-})
-
-export default function Landing() { 
-    const classes = useStyles();
-    //remember to put logo to the let 
-    return (
-        <div className='header'>
-            <AppBar position='sticky' style={{backgroundColor: "tan", fontFamily: 'Nunito'}}>
-            {/* // className={classes.appbar} elevation={0} */}
-                <Toolbar className={classes.appbarWrapper}>
-                <h1>Lingo<span className={classes.colorText}>mates👋🏽</span></h1> 
-                <IconButton>
-                    {/* <SortIcon/> */}
-                </IconButton>
-                </Toolbar>
-            </AppBar>
-
-        </div>
-    )
-}
+  return (
+    <header className="w-full h-auto bg-transparent overflow-x-hidden fixed z-50 top-0 left-0">
+      <Slide direction="down">
+    
+          <nav className={`w-full md:h-24 h-20 ${navBarColor ? "bg-white" : "bg-transparent"} lg:px-24 md:px-12 px-8 flex justify-between items-center`}>
+            <h1 className='poppin'>Lingo<span className='mates'>mates🐒</span></h1>
+            <div class="lg:flex hidden items-center gap-20">
+            <ul class="flex items-center justify-center gap-6">
+                  <li class="w-full"><button type="button" class="before:bottom-0 border-b-2 border-transparent hover:border-gray-950 py-2 px-8 relative z-10 before:content-[''] before:absolute before:left-0 before:w-full before:h-0 before:bg-color2 before:-z-10 hover:before:h-full before:transition-all before:duration-300 before:ease-in text-base">Login</button></li>
+                  <li class="w-full"><button type="button" class="border-2 border-gray-950 before:top-0 py-2 px-8 relative z-10 before:content-[''] before:absolute before:left-0 before:w-full before:h-0 before:bg-color2 before:-z-10 hover:before:h-full before:transition-all before:duration-300 before:ease-in text-base">Signup</button></li>
+                  <li class="text-gray-950">
+            {/* <select class="border-none font-light text-base outline-none bg-transparent">
+              <option value="EN">EN</option>
+              <option value="ITA">ITA</option>
+              <option value="FRA">FRA</option>
+      </select> */}
+   </li>
+</ul>
+</div>
+</nav>
+</Slide>
+</header>
+    );
+  }
--- lingomates/lingomates-ui/src/components/Hero/Hero.jsx
+++ lingomates/lingomates-ui/src/components/Hero/Hero.jsx
@@ -1,92 +1,137 @@
 import React from "react";
+import { useEffect } from "react";
 import "./Hero.css";
-import background1 from "../images/img1.jpeg"
+import background1 from "../images/img1.jpeg";
 import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
-import { makeStyles } from '@mui/styles'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { Grid, Avatar } from '@mui/material';
-import Button from '@mui/material/Button' 
-
-import AppBar from '@mui/material/AppBar';
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { Grid, Avatar } from "@mui/material";
+import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
 
 const languageData = [
-    { name: 'English', flagSrc: 'src/assets/english.png' },
-    { name: 'Spanish', flagSrc: 'src/assets/spanish.png' },
-    { name: 'French', flagSrc: 'src/assets/french.png' },
-    { name: 'Italian', flagSrc: 'src/assets/italian.png' },
-    { name: 'Swedish', flagSrc: 'src/assets/swedish.png' },
-    { name: 'German', flagSrc: 'src/assets/german.png' },
-  ];
+  { name: "English", flagSrc: "src/assets/english.png" },
+  { name: "Spanish", flagSrc: "src/assets/spanish.png" },
+  { name: "French", flagSrc: "src/assets/french.png" },
+  { name: "Italian", flagSrc: "src/assets/italian.png" },
+  { name: "Swedish", flagSrc: "src/assets/swedish.png" },
+  { name: "German", flagSrc: "src/assets/german.png" },
+];
 
-
-export default function Hero(){
-
-    const useStyles = makeStyles({
-        root: {
-            minHeight: '100vh',
-            backgroundImage: `url(${"src/components/images/countryImg2.jpg"})`
-        },
-        
-    })    
-
-    return(
-        <div className="hero">
-        <div className="background-img">
-            <div className="white-back">
-            {/* <Link to = "/">
-            <img className='logo' src='src/assets/snail.png'/>
-            </Link> */}
-            <h1 className="title" style={{ elevation: 3 }}>LINGOMATES</h1>
-            <p className="para">Connect. Chat. Learn</p>
-            <Link to = "/register">
-            <Button variant="contained">Get Started</Button>
-            </Link>
-            <Link to = "/login">
-            <Button variant="contained">Already have an account?</Button>
-            </Link>
-            </div>
+export default function Hero() {
+  return (
+  <div className="hero">
+    <div className="opacity">
+      <div className="background-img">
+        <div className="white-back">
+          <h1 className="title">Lingo<span className="mates">mates👋🏽</span></h1>
+          <p className="para">A fun and sociable way to learn a new language.</p>
+          <Link to="/register">
+            <button className="myButton">Get Started</button>
+          </Link>
+          <Link to="/login">
+            <button className="myButton">Already have an account?</button>
+          </Link>
         </div>
-        <div className="about-us" style={{ marginTop: "-2rem"}}>
-      <h2>Who Are We?</h2>
-      <p>
-        At Lingomates, we are a team of language enthusiasts who believe in the
-        power of human connection and social learning. Our mission is to make
-        language learning fun, interactive, and sociable for everyone. We
-        understand that learning a new language can be challenging, and that's
-        why we've created this platform to help language learners connect with
-        each other to practice and improve their language skills.
-      </p>
-      <h2>What Is the Point?</h2>
-      <p>
-        The point of Lingomates is to provide a free and sociable platform for
-        language learners from all backgrounds to come together and learn from
-        each other. We want to break down barriers and make language learning a
-        positive and enjoyable experience. Whether you're a language enthusiast,
-        a beginner, or just looking for a casual way to practice, Lingomates is
-        here to help you achieve your language goals.
-      </p>
-      <h2>Why Choose Lingomates?</h2>
-      <p>
-        Unlike other language learning apps, Lingomates focuses on the power of
-        human interaction. Our platform allows you to connect with other users
-        through chat rooms, so you can practice your language
-        skills in a real-life setting. You can also find language partners who
-        are at a similar proficiency level, allowing you to learn and grow
-        together. We also offer a text translator feature, so you can
-        communicate with users who speak a different language. Lingomates is
-        flexible and adaptable to your language journey, providing a supportive
-        and engaging community for all language learners.
-      </p>
-    </div>
-    <div className="cardContainer" style={{ marginTop: "-1rem"}}>
+      </div>
+      </div>
+
+      <div className="about-us">
+        <div className="about-us-left">
+          <h2>Who Are We?💭</h2>
+          <p>
+            At Lingomates, we are a team of language enthusiasts who believe
+            in the power of human connection and social learning. Our mission
+            is to make language learning fun, interactive, and sociable for
+            everyone. We understand that learning a new language can be
+            challenging, and that's why we've created this platform to help
+            language learners connect with each other to practice and improve
+            their language skills.
+          </p>
+        </div>
+        <div className="about-us-right">
+          <Card sx={{ maxWidth: 400 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="200"
+                image= "https://languagelifeschool.com/wp-content/uploads/2021/11/ll1_compressed.jpg"
+                alt="About Us"
+              />
+            </CardActionArea>
+          </Card>
+        </div>
+      </div>
+
+      <div className="about-us">
+        <div className="about-us-left">
+          <Card sx={{ maxWidth: 400 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="200"
+                image="https://www.altalang.com/wp-content/uploads/2022/10/portrait-of-enthusiastic-business-people-in-circle-picture-id513439341.jpg"                alt="What's the Point"
+              />
+            </CardActionArea>
+          </Card>
+        </div>
+        <div className="about-us-right">
+          <h2>What Is the Point?☝🏽</h2>
+          <p>
+            Many language learning apps like Duolingo are excellent for
+            individual learning, but they lack social interaction. This can be
+            demotivating for users, and they may feel like they are not
+            accomplishing their language learning goals. At Lingomates, we
+            focus on the power of human interaction. Our platform allows you
+            to connect with other users through chat rooms, so you can
+            practice your language skills in a real-life setting. You can also
+            find language partners who are at a similar proficiency level,
+            allowing you to learn and grow together. We believe that social
+            interaction is a key component of language learning success, and
+            our platform is designed to provide a supportive and engaging
+            community for all language learners.
+          </p>
+        </div>
+      </div>
+
+      <div className="about-us">
+        <div className="about-us-left">
+          <h2>Why Choose Lingomates?🙈</h2>
+          <p>
+            Unlike other language learning apps, Lingomates focuses on the
+            power of human interaction. Our platform allows you to connect
+            with other users through chat rooms, so you can practice your
+            language skills in a real-life setting. You can also find language
+            partners who are at a similar proficiency level, allowing you to
+            learn and grow together. We also offer a text translator feature,
+            so you can communicate with users who speak a different language.
+            Lingomates is flexible and adaptable to your language journey,
+            providing a supportive and engaging community for all language
+            learners.
+          </p>
+        </div>
+        <div className="about-us-right">
+          <Card sx={{ maxWidth: 400 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="200"
+                image="https://st2.depositphotos.com/1809585/48354/i/450/depositphotos_483547150-stock-photo-happy-woman-smiling-checking-smart.jpg"
+                alt="Why Choose Us"
+              />
+            </CardActionArea>
+          </Card>
+        </div>
+      </div>
+
+      <div className="cardContainer">
+        {/* <h2>Did you know...</h2> */}
         <div className="facts">
-          <Card sx={{ maxWidth: 345, backgroundColor: "tan" }}>
+          <Card sx={{ maxWidth: 345, backgroundColor: "white" }}>
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -95,10 +140,10 @@ export default function Hero(){
                 alt="Language Learning"
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div" fontFamily={'Nunito'}>
+                <Typography gutterBottom variant="h5" component="div" fontFamily={'Poppins'}>
                   Practicing Speaking Increases Fluency
                 </Typography>
-                <Typography variant="body2" color="text.secondary" fontFamily={'Nunito'}>
+                <Typography variant="body2" color="text.secondary" fontFamily={'Poppins'}>
                   Research has shown that active speaking and regular practice in a language play a crucial role in achieving fluency. Engaging in conversations, whether with native speakers or other language learners, helps reinforce vocabulary, improve pronunciation, and develop natural language usage. Regular speaking practice can significantly boost language proficiency and make learners more confident in using the language in various contexts.
                 </Typography>
               </CardContent>
@@ -106,7 +151,7 @@ export default function Hero(){
           </Card>
         </div>
         <div className="fact2">
-          <Card sx={{ maxWidth: 345,  backgroundColor: "tan" }}>
+          <Card sx={{ maxWidth: 345,  backgroundColor: "white" }}>
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -115,25 +160,35 @@ export default function Hero(){
                 alt="Language Learners Interaction"
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div" fontFamily={'Nunito'}>
+                <Typography gutterBottom variant="h5" component="div" fontFamily={'Poppins'}>
                   Multilingualism and Brain Health
                 </Typography>
-                <Typography variant="body2" color="text.secondary" fontFamily={'Nunito'}>
+                <Typography variant="body2" color="text.secondary" fontFamily={'Poppins'}>
                   Besides the obvious benefits of communication and cultural understanding, being multilingual can have positive effects on brain health. Studies have suggested that speaking multiple languages may delay the onset of dementia and Alzheimer's disease in older adults. The constant exercise of the brain while switching between languages seems to contribute to improved cognitive function, memory, and problem-solving abilities, offering long-term benefits beyond language skills.
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
         </div>
-        <Grid container spacing={2} justifyContent="center" alignItems="center" backgroundColor = "tan">
-      {languageData.map((language, index) => (
-        <Grid item key={index}>
-          <Avatar alt={language.name} src={language.flagSrc} sx={{ width: 50, height: 50 }} />
-          <p>{language.name}</p>
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+          backgroundColor="white"
+        >
+          {languageData.map((language, index) => (
+            <Grid item key={index}>
+              <Avatar
+                alt={language.name}
+                src={language.flagSrc}
+                sx={{ width: 50, height: 50 }}
+              />
+              <p className="flag-font">{language.name}</p>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
       </div>
-        </div>
-    )
+    </div>
+  );
 }
