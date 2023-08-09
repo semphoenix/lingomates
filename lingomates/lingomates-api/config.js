a239607e055a0241bdffd7ffd2515f894043ca47
Mikyas Kebede <mikyasyesigat@gmail.com>
2023-08-08 18:38:38 -0700

    updated config and db.js for deployment 6:38pm

--- lingomates/lingomates-api/config.js
+++ lingomates/lingomates-api/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config
+require("dotenv").config()
 //require("colors")
 
 //to use values from .env need to put them into this folder
@@ -7,14 +7,14 @@ const PORT = process.env.PORT ? Number(process.env.PORT):3001
 function getDatabaseUri(){
    const dbUser = process.env.DATABASE_USER || "postgres"
    const dbPass = process.env.DATABASE_PASS? encodeURI(process.env.DATABASE_PASS) : "postgres" //checks if password does exist then encode it
-   const dbHost = process.env. DATABASE_HOST || "localhost"
+   const dbHost = process.env.DATABASE_HOST || "localhost"
    const dbPort = process.env.DATABASE_PORT || "5432"
    const dbName = process.env.DATABASE_NAME || "lingomate"
    // DATABASE_TEST_NAME=lifetracker_test
 
    const dbHostedURL = process.env.DATABASE_HOSTED_URL
    //if user supplies database url use that else make it yourself
-  return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}` //full database connection string
+  return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?ssl=true` //full database connection string
 
 }
 const BCRYPT_WORK_FACTOR=13
