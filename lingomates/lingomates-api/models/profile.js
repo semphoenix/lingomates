fb354b9315c717ef9f76f0ccf0770cad972ae039
semphoenix <rodriguezs2021@gmail.com>
2023-08-10 13:45:50 -0700

    fixing postgres query

--- lingomates/lingomates-api/models/profile.js
+++ lingomates/lingomates-api/models/profile.js
@@ -5,19 +5,20 @@ const { BadRequestError, UnauthorizedError } = require("../utils/errors");
 class Profile {
   static async user_Language_prof(userIdRequested) {
     console.log("In Profile model");
-    const result = await db.query(`
+    console.log(userIdRequested);
+    const allUsers = await db.query(`
     SELECT * from users;`);
-    // const result = await db.query(
-    //   `
-    //     SELECT u.first_name, u.id, u.last_name,u.profilePicture, u.email, u.nativeLanguage, l.linguaName, l.countryFlag,
-    //     l.imageUrl, ul.proficiencyLevel, u.username
-    //     FROM users AS u
-    //     INNER JOIN userLingua ul ON u.id = ul.userId
-    //     INNER JOIN lingua l ON ul.linguaId = l.id
-    //     WHERE u.id = $1;`,
-    //   [userIdRequested]
-    // );
-    console.log(result);
+    const result = await db.query(
+      `
+        SELECT u.first_name, u.id, u.last_name,u.profilePicture, u.email, u.nativeLanguage, l.linguaName, l.countryFlag,
+        l.imageUrl, ul.proficiencyLevel, u.username
+        FROM users AS u
+        INNER JOIN userLingua ul ON u.id = ul.userId
+        INNER JOIN lingua l ON ul.linguaId = l.id
+        WHERE u.id = $1;`,
+      [userIdRequested]
+    );
+    console.log(allUsers);
     const userLinguaProf = result.rows;
     return userLinguaProf;
   }
